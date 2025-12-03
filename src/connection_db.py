3c1bc80913c1e60d9b671da025f50d50d4efc02f
Rodrigo Cordeiro Soares <rodrigo.cordeiro.bkp@gmail.com>
2025-12-02 21:03:24 -0300

    adicionando suporte para st.secrets

--- src/connection_db.py
+++ src/connection_db.py
@@ -6,12 +6,17 @@ from dotenv import load_dotenv
 load_dotenv()  # carrega .env se existir
 
 def get_db():
-    uri = os.getenv("MONGO_URI")
-    db_name = os.getenv("MONGO_DB")
+    try:
+        # Primeiro tenta pegar do Streamlit Secrets
+        uri = st.secrets["MONGODB"]["URI"]
+        db_name = st.secrets["MONGODB"]["DB"]
+    except Exception:
+        # Se não estiver no Streamlit Cloud, usa variáveis de ambiente
+        uri = os.getenv("MONGO_URI")
+        db_name = os.getenv("MONGO_DB")
 
     client = MongoClient(uri)
     return client[db_name]
-
 
 def get_collection(nome_colecao):
     db = get_db()
